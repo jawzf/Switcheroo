2d7b0b0b71bb0963b65155fa55571aed1ed0ec9a
jawzf <jokuriackal@gmail.com>
2018-05-28 15:40:12 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Switcheroo
-Export your Music Libraries from different streaming service to Spotify!
+Import your personal libraries from streaming services to Spotify!
+
+<h3>A free service packed in a Chrome Extension</h3>
+This is a chrome extension under development. <br>
+<p>It currently supports, exporting your library from Google Play Music to Spotify. It can be found <a href="https://chrome.google.com/webstore/detail/switcheroo/pnmhhafmkpbmbiibonckcnomganolpfa">here<a>.</p>
+<br>
+Support for more streaming services coming soon.<br>
+
+<b>Services Available<b><br>
+[#] Google Play Music<br>
+
+<b>Coming Soon</b><br>
+[] Saavn<br>
+[] Gaana<br>
+[] Apple Music<br>
+[] many more..
---
+++ src/exporter.js
@@ -0,0 +1,81 @@
+var j = 0;
+var mainContainer = document.getElementById("mainContainer");
+console.log(mainContainer);
+
+var playlistJson = [];
+var FINAL_JSON = [];
+
+
+function mainCode() {
+    mainContainer.scrollTop = 0;
+    codeRunner();
+}
+
+
+function codeRunner() {
+
+    if (j < 15188) {
+        var t1 = setTimeout(function() {
+            exportPlaylistCode();
+        }, 1000);
+    } else {
+        //console.log(playlistJson);
+        for (var d = 0; d < playlistJson.length; d++) {
+            if (findIdInArray(playlistJson[d].id, FINAL_JSON)) {
+                FINAL_JSON.push(playlistJson[d]);
+            }
+        }
+
+        localStorage.setItem("GPLAY_LIB", JSON.stringify(FINAL_JSON));
+        chrome.runtime.sendMessage({
+            greeting: "gplay library",
+            GPLAY_LIB: JSON.stringify(FINAL_JSON)
+        }, function(response) {
+            console.log(response.farewell);
+        });
+
+    }
+
+
+}
+
+function exportPlaylistCode() {
+    mainContainer.scrollTop = 350 + j;
+    var tm = setTimeout(function() {
+        var playlist = document.querySelectorAll('.song-table tr.song-row');
+        for (var i = 0; i < playlist.length; i++) {
+            var l = playlist[i];
+            var id = l.getAttribute("data-index");
+            var title = l.querySelectorAll('td[data-col="title"] .column-content')[0].textContent;
+            var artist = l.querySelectorAll('td[data-col="artist"] .column-content')[0].textContent;
+            var album = l.querySelectorAll('td[data-col="album"] .column-content')[0].textContent;
+
+            //console.log(artist + '|' + title + '|' + album);
+            addTrackToJson(playlistJson, id, title, artist, album);
+        }
+    }, 1000);
+    j = j + 500;
+    codeRunner();
+}
+
+mainCode();
+
+
+function addTrackToJson(json1, id, title, artist, album) {
+    var temp = {};
+    temp['id'] = id;
+    temp['title'] = title;
+    temp['artist'] = artist;
+    temp['album'] = album;
+    json1.push(temp);
+    return json1;
+}
+
+function findIdInArray(id, json2) {
+    for (var i = 0; i < json2.length; i++) {
+        if (id == json2[i].id) {
+            return false;
+        }
+    }
+    return true;
+}
